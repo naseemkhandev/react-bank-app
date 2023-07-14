968bdff4fee08e4b1707a570c31d6c81b860c63a
Naseem Khan <nk8257949@gmail.com>
2023-07-14 14:11:12 -0700

    payment method section added

--- src/components/CommonTitle.jsx
+++ src/components/CommonTitle.jsx
@@ -1,6 +1,6 @@
 const CommonTitle = ({ title }) => {
   return (
-    <h1 className="text-3xl ss:text-4xl md:text-5xl leading-normal sm:leading-relaxed font-semibold">
+    <h1 className="text-3xl ss:text-4xl md:text-5xl leading-normal sm:leading-relaxed md:leading-relaxed font-semibold">
       {title}
     </h1>
   );
