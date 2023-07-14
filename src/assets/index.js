d3a2dbb7f66eb082d9a9b4c57bb4ff154432cc2e
Naseem Khan <nk8257949@gmail.com>
2023-07-14 14:14:46 -0700

    card payment method added

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,5 @@ import discount from "./Discount.svg";
 import bill from "./bill.png";
 import appstore from "./appstore.svg";
 import playstore from "./playstore.svg";
-import payment from "./payment.png";
 
-export { robot, discount, bill, appstore, playstore, payment };
+export { robot, discount, bill, appstore, playstore };
